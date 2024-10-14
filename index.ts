78ef484950c8d561aef77e8990dea30c6d27f542
samini <soufianeamini2002@gmail.com>
2024-10-14 16:52:27 +0100

    in progress

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import { input, select } from "@inquirer/prompts"
+import assert from "assert"
 
 type PokemonEntry = {
   name: string
   url: string
+}
+
+async function loadPokemon(url: string): Promise<Pokemon> {
+  return (await fetch(url).then((res) => res.json())) as Pokemon
 }
 
 async function loadPokemons(): Promise<PokemonEntry[]> {
@@ -13,18 +18,19 @@ async function loadPokemons(): Promise<PokemonEntry[]> {
   return pokemons
 }
 
-async function selectPokemon(pokemons: PokemonEntry[]) {
+async function selectPokemon(pokemons: PokemonEntry[]): Promise<PokemonEntry> {
   let pokemonFound = false
+  let finalPokemon: PokemonEntry | undefined = undefined
 
   while (!pokemonFound) {
     const pokemonName = await input({
       message: "What pokemon do you want to use?",
     })
 
-    const pokemon = pokemons.find((p) => p.name === pokemonName.toLowerCase())
-    if (pokemon) {
+    finalPokemon = pokemons.find((p) => p.name === pokemonName.toLowerCase())
+    if (finalPokemon) {
       console.log("Pokemon found!")
-      console.log({ pokemon })
+      console.log({ pokemon: finalPokemon })
       console.log("Do you want to use this pokemon?")
       pokemonFound = await select({
         message: "Do you want to use this pokemon?",
@@ -34,13 +40,15 @@ async function selectPokemon(pokemons: PokemonEntry[]) {
         ],
       })
     } else {
-      const pokemon = pokemons.find((p) =>
+      finalPokemon = pokemons.find((p) =>
         p.name.includes(pokemonName.toLowerCase())
       )
 
-      if (pokemon) {
+      if (finalPokemon) {
+        console.log("Similarly named Pokemon found!")
+        console.log({ pokemon: finalPokemon })
         pokemonFound = await select({
-          message: `Did you mean to choose ${pokemon.name}?`,
+          message: `Did you mean to choose ${finalPokemon.name}?`,
           choices: [
             { name: "yes", value: true },
             { name: "no", value: false },
@@ -49,12 +57,44 @@ async function selectPokemon(pokemons: PokemonEntry[]) {
       }
     }
   }
+
+  assert(finalPokemon !== undefined)
+  return finalPokemon as PokemonEntry
 }
+
+type Pokemon = {
+  name: string
+  stats: {
+    base_stat: number
+    effort: number
+    stat: {
+      name: string
+      url: string
+    }
+  }[]
+  moves: {
+    move: {
+      name: string
+      url: string
+    }
+  }
+}
+
+async function loadStats(pokemon: Pokemon) {
+	const statsToFetch = ["hp", "attack"]
+	const stats = pokemon.stats.filter(stat)
+}
+
+async function loadMoves(pokemon: Pokemon) {}
 
 async function main() {
   console.log("Please wait, fetching pokemon data from api..")
   const pokemons = await loadPokemons()
-  const selectedPokemon = selectPokemon(pokemons)
+  const selectedPokemon = await selectPokemon(pokemons)
+  const activePokemon: Pokemon = await loadPokemon(selectedPokemon.url)
+  const moves: any = await loadMoves(activePokemon)
+  const stats = loadStats(activePokemon)
+  // const moves = randomMovesSelection(activePokemon.moves)
 }
 
 main()
